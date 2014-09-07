21fd0f231cba87664c551b6d5baf28b8ed338cc5
Kayne Middleton <kayne@anytech.co.nz>
2014-09-07 21:58:36 +1200

    Create GroupedSelect2Field.php
    Covers GroupedDropdownField

---
+++ code/GroupedSelect2Field.php
@@ -0,0 +1,13 @@
+<?php 
+class GroupedSelect2Field extends GroupedDropdownField {
+
+	function Field($properties = array()){
+		Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
+		Requirements::javascript(THIRDPARTY_DIR . '/jquery-entwine/dist/jquery.entwine-dist.js');
+		Requirements::javascript(SELECT2_MODULE . "/select2/select2.js");
+		Requirements::javascript(SELECT2_MODULE . "/javascript/select2.init.js");
+		Requirements::css(SELECT2_MODULE . "/select2/select2.css");
+		$this->addExtraClass('select2')->addExtraClass('no-chzn');
+		return parent::Field($properties);
+	}
+}
