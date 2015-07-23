ad637e240271452a3268f71c5a47b231a9cff9b6
Shea <shea@livesource.co.nz>
2015-07-23 23:22:21 +1200

    started upgrade to select2 4.0

--- code/Select2Field.php
+++ code/Select2Field.php
@@ -23,13 +23,12 @@ class Select2Field extends DropdownField{
 		return $this->setAttribute('data-search-threshold', $num);		
 	}
 
-
 	function Field($properties = array()){
 		Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
 		Requirements::javascript(THIRDPARTY_DIR . '/jquery-entwine/dist/jquery.entwine-dist.js');
-		Requirements::javascript(SELECT2_MODULE . "/select2/select2.js");
+		Requirements::javascript(SELECT2_MODULE . "/select2/js/select2.min.js");
 		Requirements::javascript(SELECT2_MODULE . "/javascript/select2.init.js");
-		Requirements::css(SELECT2_MODULE . "/select2/select2.min.css");
+		Requirements::css(SELECT2_MODULE . "/select2/css/select2.min.css");
 		$this->addExtraClass('select2')->addExtraClass('no-chzn');
 
 		if(!$this->getAttribute('data-search-threshold')) {
