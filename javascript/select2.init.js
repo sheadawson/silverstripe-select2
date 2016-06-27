0227c24d449c137c068e920d0c7947233b0fb8ec
deracs <michael@deracs.com>
2016-06-27 18:11:48 +1200

    Update select2.init.js

--- javascript/select2.init.js
+++ javascript/select2.init.js
@@ -2,8 +2,13 @@ jQuery.entwine("select2", function($) {
 
 	$("select.select2").entwine({
 		onmatch: function() {
-			var self = this;
-			self.select2();
+			var self = this,
+			    limit = self.attr('limit');
+			if(limit > 0){
+			    self.select2({maximumSelectionSize: limit});
+			} else {
+			    self.select2();
+			}
 		},
 	});
 });
